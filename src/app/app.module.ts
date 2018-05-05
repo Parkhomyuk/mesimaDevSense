caa3fceaaa6c9830d2febfe4bdaa7e46194f3adc
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2018-05-05 14:20:04 +0300

    staffed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { PictureComponent } from './picture/picture.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule }   from '@angular/common/http';
+import { RouterModule, Routes} from "@angular/router";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PictureComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(<Routes>[
+      {path: 'gallary/:id', component: PictureComponent},
+      {path: 'home', component: HomeComponent,  useAsDefault:true},
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
